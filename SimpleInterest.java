81b61c760dbbad106add10021e10c14ff9b5ccc9
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-07-17 12:24:16 +0530

    Create SimpleInterest.java

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       int p=sc.nextInt();
+       int t=sc.nextInt();
+       int r=sc.nextInt();
+       int mul=(p*r*t)/100;
+       System.out.println(mul);
+    }
+}
